37a9b0837cd8c074ae0fe0671fd3b6bcb8d8fcea
Claudio <climoralesg@gmail.com>
2022-05-17 20:27:03 -0400

    Develop: Readme y correccion de nombre de variables

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# jwt-auth
+
+# Token Auth
+
+Api que permite la obtencion de una access token a partir de una llave maestra.
+
+
+
+
+## Librerias
+
+- express: ^4.18.0,
+- jsonwebtoken: ^8.5.1
+
+
+
+## Instalación
+
+Pasos para la Instalación
+
+```bash
+  cd jwt-auth
+  npm i
+  npm run start
+```
+
+
+    
+## Opciones de API
+
+#### Crear un token
+
+```http
+  GET /aplication/createtoken
+```
+
+#### Verificar la existencia de un token
+
+```http
+  GET /aplication/verifytoken
+```
+
+| Parameter | Type     | Description                       |
+| :-------- | :------- | :-------------------------------- |
+| `access-token`      | `header` | **Requerido**. KEY-Token |
+
+
+## Autor
+
+- [@climoralesg](https://climoralesg.dev/)
+
--- src/controllers/jwt.js
+++ src/controllers/jwt.js
@@ -3,29 +3,25 @@ const {response} = require("express");
 const {request} = require("express");
 
 
-
 const create=(req = request,res = response)=>{
+    
     const payload = {
         check:  true
     };
     const token=jwt.sign(payload,process.env.KEY);
-    res.json({respuesta: "Token creado",nombreUsuario:req.body.userName,password:req.body.password,token:token});
+    res.json({respuesta: "Token Creado",token:token});
+
 }
 
 const verify=(req=request,res=response)=>{
     const token=req.headers['access-token'];
     jwt.verify(token,process.env.KEY,(err,decode)=>{
         if(err){
-            res.json({respuesta: "Token incorrecto",userName:req.body.userName,password:req.body.password,token:token,llave:process.env.KEY}) ;
+            res.json({message: "Token Incorrecto",status:400,token:token}) ;
         }else{
-            res.json({respuesta: "Si token correcto",userName:req.body.userName,password:req.body.password,token:token,llave:process.env.KEY,decode:decode}) ;
+            res.json({message: "Token Correcto",status:200,token:token,decode:decode}) ;
         }
     });
-
-    
 }
-
-
-
 
 module.exports = {create,verify};
