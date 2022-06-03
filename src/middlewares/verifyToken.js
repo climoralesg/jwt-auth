a61a5df240b94c721775d0374b3437bb2eeb8d33
Claudio <climoralesg@gmail.com>
2022-06-02 23:42:53 -0400

    Develop: Se agrega middleware ValidateInput

---
+++ src/middlewares/verifyToken.js
@@ -0,0 +1,7 @@
+const verifyToken=()=>{
+
+}
+
+exports.module={
+    verifyToken
+}
--- src/routes/login.js
+++ src/routes/login.js
@@ -1,11 +1,12 @@
 const {Router}=require('express');
 const {login}=require('../controllers/login');
 const {changePasswordBcrypt}=require('../controllers/login');
+const {validateInputs}=require('../middlewares/validateInputs');
 
 
 const router=Router();
 
-router.get("/login",[],login);
+router.get("/login",[validateInputs],login);
 router.get("/changepasswordbcrypt",[],changePasswordBcrypt);
 
 
